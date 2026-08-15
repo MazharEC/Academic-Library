64c9ae1f69276d1ce60fa4c40c76b9cf07a62b52
Mazhar <mazhartuhin8291@gmail.com>
2026-08-15 23:39:57 +0600

    Dependences

---
+++ app/src/main/java/com/appsv/academiclibrary/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.appsv.academiclibrary
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.appsv.academiclibrary.ui.theme.AcademicLibraryTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AcademicLibraryTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
+
